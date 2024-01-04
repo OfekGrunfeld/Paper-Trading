df32e848a83333118c7a0f54a564e27bc8b1e410
OfekGrunfeld <ofekgrun@gmail.com>
2024-01-04 20:41:53 +0200

    database_models.py: added email requirements.json: updated server.py: added user deletion and some upgrades

--- database_models.py
+++ database_models.py
@@ -1,9 +1,10 @@
 import sqlalchemy as db
 from database import Base
 
-class Userbase(Base):
+class Users(Base):
     __tablename__ = 'userbase'
-
+    
     id = db.Column(db.Integer, primary_key=True, index=True)
+    email = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
--- server.py
+++ server.py
@@ -1,75 +1,105 @@
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
-
 from sqlalchemy.orm import Session
 import server_protocol as sp
 import database_models
 from database import engine, Session
 from pydantic import BaseModel, Field
+from typing import Generator
 
 # CONSTANTS
 HOST_IP = sp.Constants.HOST_IP.value
 HOST_PORT = sp.Constants.HOST_PORT.value
 
-# create app
+# Create app
 papertrading_app = FastAPI()
-# create database, connect to engine
+# Create database, Connect to engine
 database_models.Base.metadata.create_all(bind=engine)
 
-def get_db():
+# Get database
+def get_db() -> Generator[Session, any, None]:
     try:
         db = Session()
+        db = db.query(database_models.Users).filter(database_models.Users.id).first()
         yield db
     finally:
         db.close()
 
-class User(BaseModel):
-    username: str = Field(min_length=4, max_length=50)
-    password: str = Field(min_length=8, max_length=50)
+def does_username_and_password_match(user_model, username: str, password: str):
+    """
+    Checks if the username and password entered match the ones in the database
+    
+    Parameters:
+        user_model (database_models.Users): An existing user in the database
+        username (str): username
+        password (str): password
+    
+    Returns:
+        True: if the encoded username matches the user's username in the database and the encoded password matches the user's password in the database
+    """
+    try:
+        return sp.encode_string(username) == user_model.username and sp.encode_string(password) == user_model.password
+    finally:
+        return False
 
 @papertrading_app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @papertrading_app.get("/database")
-def Get_Database(db: Session = Depends(get_db)):
-    return db.query(database_models.Userbase).all()
+def get_whole_database(db: Session = Depends(get_db)):
+    return db.query(database_models.Users).all()
 
 # working fine
 @papertrading_app.post("/sign_up")
 def sign_up(username: str, password: str, db: Session = Depends(get_db)):
     try: 
-        user_model = database_models.Userbase()
+        user_model = database_models.Users()
         # save username and password encoded sha256
         user_model.username = sp.encode_string(username)
         user_model.password = sp.encode_string(password)
 
-        # add userbase to database
+        # add Users to database
         db.add(user_model)
         db.commit()
 
         return "signed up successfully"
     except Exception as error:
-        pass
+        return f"Failed to sign up {error}"
 
-# working fine
-@papertrading_app.put("/update_user/{user_id}")
-def sign_up(user_id: int, username: str, password: str, new_username: str = None, new_password: str = None, db: Session = Depends(get_db)):
+@papertrading_app.delete("/delete_user")
+def delete_user(user_id: str, username: str, password: str, db: Session = Depends(get_db)):
     try: 
-        print(f"DATA: \nUser ID: {user_id}\n Username: {username}, Password: {password}\nnew_username: {new_username}, new_password: {new_password}")
         # get user with id from database
-        user_model = db.query(database_models.Userbase).filter(database_models.Userbase.id == user_id).first()
+        user_model = db.query(database_models.Users).filter(database_models.Users.id == user_id).first()
 
         if user_model is None:
             raise HTTPException(
                 status_code=404,
                 detail=f"ID: {user_id} does not exist"
             )
-        encoded_username = sp.encode_string(username)
-        encoded_password = sp.encode_string(password)
+        if does_username_and_password_match(user_model, username, password):
+            # delete user
+            db.query(database_models.Users).filter(database_models.Users.id == user_id).delete()
+            db.commit()
+        return "Deleted user successfully"
+    except Exception as error:
+        return f"Failed to delete user {error}"
 
-        if encoded_username == user_model.username and encoded_password == user_model.password:
-            print("entered if")
+# working fine
+@papertrading_app.put("/update_user")
+def update_user(user_id: int, username: str, password: str, new_username: str = None, new_password: str = None, db: Session = Depends(get_db)):
+    try: 
+        # get user with id from database
+        user_model = db.query(database_models.Users).filter(database_models.Users.id == user_id).first()
+
+        if user_model is None:
+            raise HTTPException(
+                status_code=404,
+                detail=f"ID: {user_id} does not exist"
+            )
+
+        if does_username_and_password_match(user_model, username, password):
             # for now - don't allow changing the username and password at the same time 
             if new_username is not None and new_password is not None:
                 return "Cannot change both username and password"
@@ -81,20 +111,15 @@ def sign_up(user_id: int, username: str, password: str, new_username: str = None
                 encoded_new_password = sp.encode_string(new_password)
                 user_model.password = encoded_new_password
 
-            # add userbase to database
+            # add Users to database
             db.add(user_model)
             db.commit()
 
-            return "Update user successfully"
+            return f"Updated user {username} successfully"
         return "Username and password do not match with existing user"
     except Exception as error:
         return f"Failed to update user {error}"
-
-
     
-
-    
-
 """
 @papertrading_app.get('/Sign_out')
 def Sign_out(username: str, password: str):
